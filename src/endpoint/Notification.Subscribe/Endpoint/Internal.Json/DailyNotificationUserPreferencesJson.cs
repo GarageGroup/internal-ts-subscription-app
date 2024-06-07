4cda3f2f9b1ca28a80a87d66795638550d78bddd
Pavel Moskovoy <p@moskovoi.ru>
2024-06-07 23:21:03 +0300

    Update DailyNotificationUserPreferencesJson.cs

--- src/endpoint/Notification.Subscribe/Endpoint/Internal.Json/DailyNotificationUserPreferencesJson.cs
+++ src/endpoint/Notification.Subscribe/Endpoint/Internal.Json/DailyNotificationUserPreferencesJson.cs
@@ -5,15 +5,12 @@ namespace GarageGroup.Internal.Timesheet;
 internal sealed record class DailyNotificationUserPreferencesJson
 {
     public static DailyNotificationUserPreferencesJson Parse(DailyNotificationUserPreference userPreference)
-    {
-        ArgumentNullException.ThrowIfNull(userPreference);
-        
-        return new()
-        {
-            WorkedHours = userPreference.WorkedHours,
-            FlowRuntime = userPreference.FlowRuntime
-        };
-    }
+      =>
+      new()
+      {
+        WorkedHours = userPreference.WorkedHours,
+        FlowRuntime = userPreference.FlowRuntime
+      };
     
     public int WorkedHours { get; init; }
     

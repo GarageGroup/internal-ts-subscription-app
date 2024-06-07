df6fe894627d557ac7557bcf9145d39f460a81bb
Alexey Shchagolchin <lesha.shhagolchin@mail.ru>
2024-06-07 20:52:45 +0500

    Update unit tests

--- src/endpoint/Notification.Subscribe/Test/Test.Func/NotificationSubscribeFuncTest.cs
+++ src/endpoint/Notification.Subscribe/Test/Test.Func/NotificationSubscribeFuncTest.cs
@@ -5,51 +5,34 @@ namespace GarageGroup.Internal.Timesheet.Endpoint.Notification.Subscribe.Test.Te
 
 public static partial class NotificationSubscribeFuncTest
 {
-    private static readonly NotificationSubscribeIn SomeSubscribeInput
-        =  
+    private static readonly NotificationSubscribeOption SomeNotificationSubscribeOption 
+        = 
+        new ()
+        {
+            BotId = 10
+        };
+
+    private static readonly NotificationSubscribeIn SomeSubscribeIn 
+        = 
         new(
-            botId: long.MaxValue,
-            chatId: long.MaxValue, 
+            systemUserId: Guid.Parse("ac9bdaf3-a9c0-4b8b-833a-406bf59a9fcd"),
             subscriptionData: new DailyNotificationSubscriptionData(null));
     
-    private static Mock<IDataverseApiClient> BuildMockDataverseApi()
+    private static Mock<IDataverseApiClient> BuildMockDataverseApi(
+        Result<DataverseEntityGetOut<TelegramBotUserJson>, Failure<DataverseFailureCode>> botUserGetResult,
+        Result<DataverseEntityGetOut<NotificationTypeJson>, Failure<DataverseFailureCode>> notificationTypeGetResult,
+        Result<Unit, Failure<DataverseFailureCode>> subscriptionUpdateResult)
     {
         var mock = new Mock<IDataverseApiClient>();
         
         mock.Setup(x => x.GetEntityAsync<TelegramBotUserJson>(It.IsAny<DataverseEntityGetIn>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync(new DataverseEntityGetOut<TelegramBotUserJson>(new TelegramBotUserJson()));
+            .ReturnsAsync(botUserGetResult);
 
         mock.Setup(x => x.GetEntityAsync<NotificationTypeJson>(It.IsAny<DataverseEntityGetIn>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync(new DataverseEntityGetOut<NotificationTypeJson>(new NotificationTypeJson()));
+            .ReturnsAsync(notificationTypeGetResult);
 
         mock.Setup(x => x.UpdateEntityAsync(It.IsAny<DataverseEntityUpdateIn<NotificationSubscriptionJson>>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync(Unit.Value);
-
-        return mock;
-    }
-
-    private static Mock<IDataverseApiClient> BuildMockDataverseApiToTestFindBotUser(Failure<DataverseFailureCode> failure)
-    {
-        var mock = new Mock<IDataverseApiClient>();
-
-        mock.Setup(x => x.GetEntityAsync<TelegramBotUserJson>(It.IsAny<DataverseEntityGetIn>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync(failure);
-
-        mock.Setup(x => x.GetEntityAsync<NotificationTypeJson>(It.IsAny<DataverseEntityGetIn>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync(new DataverseEntityGetOut<NotificationTypeJson>(new NotificationTypeJson()));
-        
-        return mock;
-    }
-    
-    private static Mock<IDataverseApiClient> BuildMockDataverseApiToTestFindNotificationType(Failure<DataverseFailureCode> failure)
-    {
-        var mock = new Mock<IDataverseApiClient>();
-
-        mock.Setup(x => x.GetEntityAsync<TelegramBotUserJson>(It.IsAny<DataverseEntityGetIn>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync(new DataverseEntityGetOut<TelegramBotUserJson>(new TelegramBotUserJson()));
-        
-        mock.Setup(x => x.GetEntityAsync<NotificationTypeJson>(It.IsAny<DataverseEntityGetIn>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync(failure);
+            .ReturnsAsync(subscriptionUpdateResult);
 
         return mock;
     }

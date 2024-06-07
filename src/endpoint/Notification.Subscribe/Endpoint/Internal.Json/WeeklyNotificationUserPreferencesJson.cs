6d49a2f3e08773f8412a99061ee8290573ed7de4
Pavel Moskovoy <p@moskovoi.ru>
2024-06-07 23:24:19 +0300

    Update WeeklyNotificationUserPreferencesJson.cs

--- src/endpoint/Notification.Subscribe/Endpoint/Internal.Json/WeeklyNotificationUserPreferencesJson.cs
+++ src/endpoint/Notification.Subscribe/Endpoint/Internal.Json/WeeklyNotificationUserPreferencesJson.cs
@@ -5,21 +5,17 @@ namespace GarageGroup.Internal.Timesheet;
 internal sealed record class WeeklyNotificationUserPreferencesJson
 {    
     public static WeeklyNotificationUserPreferencesJson Parse(WeeklyNotificationUserPreference userPreference)
-    {
-        ArgumentNullException.ThrowIfNull(userPreference);
-
-        return new WeeklyNotificationUserPreferencesJson
+        =>
+        new()
         {
             Weekday = string.Join(", ", userPreference.Weekday.Map(x => (int)x).ToArray()),
             FlowRuntime = userPreference.FlowRuntime,
             WorkedHours = userPreference.WorkedHours
         };
-    }
     
     public string? Weekday { get; init; }
     
     public TimeOnly FlowRuntime { get; init; }
 
-    public int WorkedHours { get; init; }
-    
+    public int WorkedHours { get; init; }   
 }

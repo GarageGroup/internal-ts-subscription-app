bf0bdd61540e205607c0badd5de89994368a24bb
Alexey Shchagolchin <lesha.shhagolchin@mail.ru>
2024-06-26 19:21:36 +0500

    Add descriptions and examples for properties of Out contract

--- src/endpoint/Subscription.GetSet/Contract/Subscription.cs
+++ src/endpoint/Subscription.GetSet/Contract/Subscription.cs
@@ -1,12 +1,20 @@
 using System;
+using GarageGroup.Infra;
 
 namespace GarageGroup.Internal.Timesheet;
 
+using static SubscriptionSetGetMetadata;
+
 public sealed record class Subscription
 {
+    [SwaggerDescription(Out.IdDescription)]
+    [StringExample(Out.IdExample)]
     public Guid Id { get; init; } 
     
+    [SwaggerDescription(Out.NotificationTypeDescription)]
+    [StringExample(Out.NotificationTypeExample)]
     public NotificationType NotificationType { get; init; }
 
+    [SwaggerDescription(Out.UserPreferenceDescription)]
     public INotificationUserPreference? UserPreference { get; init; }
 }
--- src/endpoint/Subscription.GetSet/Contract/SubscriptionSetGetMetadata.cs
+++ src/endpoint/Subscription.GetSet/Contract/SubscriptionSetGetMetadata.cs
@@ -16,4 +16,17 @@ internal static class SubscriptionSetGetMetadata
             =
             "Retrieves a list of notification subscriptions for an authenticated user.";
     }
+
+    public static class Out
+    {
+        public const string IdDescription = "Represents the unique identifier of the notification subscription.";
+
+        public const string IdExample = "8059bf1e-e3f2-426c-802e-7213c83fcf31";
+        
+        public const string NotificationTypeDescription = "Specifies the type of notification that the user is subscribed to.";
+
+        public const string NotificationTypeExample = "DailyNotification";
+        
+        public const string UserPreferenceDescription = "Represents the user's preferences or settings related to notifications of the specified type.";
+    }
 }
